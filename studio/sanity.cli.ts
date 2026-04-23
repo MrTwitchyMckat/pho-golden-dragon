9710fa8dbce4f9432774b4ac98b81ade1b02e70b
Vivek Bhardwaj <vivekbhardwajnv@gmail.com>
2026-04-23 12:30:50 -0700

    feat(sanity): add studio host

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -4,5 +4,6 @@ const projectId = process.env.SANITY_STUDIO_PROJECT_ID || '03e15w02'
 const dataset = process.env.SANITY_STUDIO_DATASET || 'production'
 
 export default defineCliConfig({
+  studioHost: 'phogoldendragon',
   api: { projectId, dataset },
 })
